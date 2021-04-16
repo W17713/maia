e083e095bc2f9fcd61a51d029672f36f618c578b
W17713 <z.armah@yahoo.com>
2021-04-16 09:14:53 -1200

    Add check staged file and committed file status

--- vcsapi/vcsmain.py
+++ vcsapi/vcsmain.py
@@ -1,4 +1,4 @@
-from flask import Flask,request
+from flask import Flask,request,jsonify
 import shutil
 import os
 
@@ -17,13 +17,34 @@ def filevcs():
         return request.json
     return 'filevcs'
 
+def move(source,destination,filename):
+    cwd = os.getcwd()
+    ext='.txt'
+    print(format(cwd))
+    shutil.move(cwd+source+filename+ext,cwd+destination+filename+ext)
+    return 'success'
 
 @app.route('/api/v1/add', methods=["POST"])
 def mount():
     filename=request.args.get('name')
-    cwd = os.getcwd()
-    ext='.txt'
-    print(format(cwd))
-    shutil.move(cwd+'\\files\\'+filename+ext,cwd+'\\staged\\'+filename+ext)
+    move('\\files\\','\\staged\\',filename)
     return 'done'
+
+
+@app.route('/api/v1/commit',methods=["POST"])
+def commit():
+    filename=request.args.get('name')
+    move('\\staged\\','\\commit\\',filename)
+    return 'done'
+
+
+@app.route('/api/v1/status',methods=["POST"])
+def status():
+    cwd=os.getcwd()
+    stagepath=cwd+'\\staged\\'
+    commitpath=cwd+'\\commit\\'
+    stagedfiles=os.listdir(stagepath)
+    commitfiles=os.listdir(commitpath)
+    return jsonify(committed_files=commitfiles,staged_files=stagedfiles)
+
 
